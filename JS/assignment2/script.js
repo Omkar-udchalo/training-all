37da53d6b8baca693caa82808388d93f7bd9e2a4
OmkarM2310 <omkar.mankar@udchalo.com>
2021-09-03 15:50:36 +0530

    added JS assignments and code logic

--- JS/assignment2/script.js
+++ JS/assignment2/script.js
@@ -5,6 +5,17 @@ const timer = document.getElementById("timer");
 let stopwatch;
 let second = 0;
 
+/*
+    LOGIC
+
+    1.new Date(second) ... this gives a string of GMT 5.30 and every time we call it it increases the time based on 
+    value of second.
+    2.so at first value of second = 1000;
+    3.now we get result as 05:30:01 i.e. 1 second has elapsed
+    4.similarly if in loop second=5000 then result is 05:30:05 i.e 5 second have elapsed
+    5.Now getUTCHours returns the hours elapsed,getUTCMin gives min and getUTCseconds give seconds; 
+*/
+
 function timeStart() {
     clearInterval(stopwatch);
     stopwatch = setInterval(() => {
